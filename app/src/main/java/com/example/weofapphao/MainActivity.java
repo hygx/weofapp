6fe127783ab9337c38c61f534b7a4a5434760b3c
wanghao <gqzcj2015>
2015-11-08 01:48:57 +0800

    包结构初建

--- app/src/main/java/com/example/weofapphao/MainActivity.java
+++ app/src/main/java/com/example/weofapphao/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends ActionBarActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/weofapphao/wanghao/HaoMainActivity.java
@@ -0,0 +1,7 @@
+package com.example.weofapphao.wanghao;
+
+/**
+ * Created by ºÆ on 2015/11/8.
+ */
+public class HaoMainActivity {
+}
