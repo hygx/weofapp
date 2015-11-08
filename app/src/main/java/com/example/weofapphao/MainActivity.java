a3a9664aa371341125177eeb4b484ee7b9d985a2
wanghao <gqzcj2015>
2015-11-08 20:04:03 +0800

    首页的再次修改

--- app/src/main/java/com/example/weofapphao/MainActivity.java
+++ app/src/main/java/com/example/weofapphao/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
 
 
@@ -27,28 +28,46 @@ public class MainActivity extends ActionBarActivity {
     private void initView() {
 
       wh = (Button) findViewById(R.id.auther1);
+        xqq = (Button) findViewById(R.id.auther2);
+        zcm = (Button) findViewById(R.id.auther3);
+        yb = (Button) findViewById(R.id.auther4);
+
+        initViewData();
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initViewData() {
+
+        wh.setOnClickListener(this);
+        xqq.setOnClickListener(this);
+        zcm.setOnClickListener(this);
+        yb.setOnClickListener(this);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.auther1://王浩的项目
+
+                break;
+
+            case R.id.auther2://许强强项目
+
+                break;
+
+            case R.id.auther3://曹智民项目
+
+                break;
+
+            case R.id.auther4://杨斌的项目
+
+                break;
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
