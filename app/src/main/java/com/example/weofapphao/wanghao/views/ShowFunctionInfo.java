c6724b29d62a216147fccfd522cb6a1496ee39f2
wanghao <gqzcj2015>
2015-11-11 01:03:21 +0800

    一个自定义展示 所作信息的 公共类 已完成

---
+++ app/src/main/java/com/example/weofapphao/wanghao/views/ShowFunctionInfo.java
@@ -0,0 +1,84 @@
+package com.example.weofapphao.wanghao.views;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.weofapphao.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by 浩 on 2015/11/10.
+ *
+ */
+public class ShowFunctionInfo extends ImageView implements View.OnClickListener {
+
+    /**
+     * 信息标记
+     */
+    private int tag ;
+
+    public ShowFunctionInfo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOnClickListener(this);
+        TypedArray  type =  context.obtainStyledAttributes(attrs, R.styleable.showfuctionattrs_tag);
+
+         tag =  Integer.parseInt( type.getString(R.styleable.showfuctionattrs_tag_infotag));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                String textInfo = getTextInfo();
+
+                String[] split = textInfo.split(" ");
+
+                new AlertDialog.Builder(getContext())
+                        .setMessage(split[tag])
+                        .setPositiveButton("确定", null)
+                        .show();
+
+                break ;
+        }
+
+        return super.onTouchEvent(event);
+
+    }
+
+    private String getTextInfo() {
+
+        InputStream inputStream = getContext().getResources().openRawResource(R.raw.dayplan);
+
+        InputStreamReader reader  = new InputStreamReader(inputStream);
+        BufferedReader bfr = new BufferedReader(reader);
+        StringBuffer sb = new StringBuffer("");
+        String line = null;
+        try {
+            while ((line = bfr.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+        }
+
+        return  sb.toString() ;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Log.e("tag","click");
+
+    }
+}
