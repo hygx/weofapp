81927226143ebca180e2b1e4ff8057ce7894c11f
liuzhonghu <379535027@qq.com>
2015-11-14 01:12:04 +0800

    自定义最简单的侧滑菜单完成

--- app/src/main/java/com/example/weofapphao/liuzhonghu/LZHActivity.java
+++ app/src/main/java/com/example/weofapphao/liuzhonghu/LZHActivity.java
@@ -1,21 +1,33 @@
 package com.example.weofapphao.liuzhonghu;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.weofapphao.R;
-import com.example.weofapphao.liuzhonghu.view.SlidingMenu;
+import com.example.weofapphao.liuzhonghu.activity.SlidingMenuActivity;
 
-public class LZHActivity extends ActionBarActivity {
+public class LZHActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-    private SlidingMenu mMenu;
-
+    private Button mSlidingMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lzh);
 
-        mMenu = (SlidingMenu) findViewById(R.id.id_menu);
+        mSlidingMenu = (Button) findViewById(R.id.myslidingmenu);
+
+        mSlidingMenu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.myslidingmenu:
+                this.startActivity(new Intent(this, SlidingMenuActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/weofapphao/liuzhonghu/activity/SlidingMenuActivity.java
@@ -0,0 +1,24 @@
+package com.example.weofapphao.liuzhonghu.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.weofapphao.R;
+import com.example.weofapphao.liuzhonghu.view.SlidingMenu;
+
+public class SlidingMenuActivity extends AppCompatActivity {
+
+    private SlidingMenu mMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding_menu);
+
+        mMenu = (SlidingMenu) findViewById(R.id.id_menu);
+    }
+
+    public void toggleMenu(View view){
+        mMenu.toggle();
+    }
+}
