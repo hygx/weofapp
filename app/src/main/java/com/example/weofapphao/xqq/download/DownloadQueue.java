ca4c19d40f562bf3ef677dccccb6ea67d3cd0cd2
Rrtoyewx <274423556@qq.com>
2015-11-15 22:06:19 +0800

    rrtoyewx  explosionFeild

--- app/src/main/java/com/example/weofapphao/xqq/download/DownloadQueue.java
+++ app/src/main/java/com/example/weofapphao/xqq/download/DownloadQueue.java
@@ -2,6 +2,8 @@ package com.example.weofapphao.xqq.download;
 
 import java.io.File;
 import java.lang.String;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.PriorityBlockingQueue;
 
 /**
@@ -11,9 +13,14 @@ import java.util.concurrent.PriorityBlockingQueue;
 public class DownloadQueue {
     public static final String TAG = DownloadQueue.class.getSimpleName();
 
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+
+    private static final ExecutorService _downloadExcutorService = Executors.newFixedThreadPool(CPU_COUNT + 1);
+
+
     private static DownloadQueue _instance;
 
-    private final PriorityBlockingQueue<DownloadTask> _downloadQueue =
+    private final static PriorityBlockingQueue<DownloadTask> _downloadQueue =
             new PriorityBlockingQueue<>();
 
     private DownloadQueue() {
@@ -30,20 +37,46 @@ public class DownloadQueue {
         }
     }
 
-    public boolean addDownloadTask(DownloadTask task) {
+    /**
+     * add task  to  queue
+     *
+     * @param task
+     * @return
+     * @throws Exception
+     */
+
+    public boolean addDownloadTask(DownloadTask task) throws Exception {
         synchronized (_downloadQueue) {
-
-            boolean exits = checkTaskInQueue(task);
-
-
+            boolean existInQueue = checkTaskInQueue(task);
+            boolean existInFile = checkTaskInFile(task);
+            if (!existInFile && !existInQueue) {
+                return _downloadQueue.add(task);
+            }
         }
-
-
         return false;
+    }
+
+    public void startDownloadTask() throws InterruptedException {
+        synchronized (_downloadQueue) {
+            while (true) {
+                DownloadTask task = _downloadQueue.take();
+                _downloadExcutorService.execute(new DownloadTaskRunnable(task));
+            }
+        }
+    }
+
+    public void stop() {
+        _downloadExcutorService.shutdownNow();
+    }
+
+    public static void restoreQueue() {
+        _downloadExcutorService.shutdownNow();
+        _downloadQueue.clear();
     }
 
     /**
      * check downloadqueue exiting
+     *
      * @param task
      * @return
      */
@@ -59,14 +92,31 @@ public class DownloadQueue {
         return false;
     }
 
-    public boolean checkTaskInFile(DownloadTask task){
-        File file = new File(DownloadTask.LOCAL_PATH,task.getFileName());
-        if(file.exists()){
-
+    public boolean checkTaskInFile(DownloadTask task) {
+        File file = new File(DownloadTask.LOCAL_PATH, task.getFileName());
+        if (file.exists()) {
+            return true;
         }
-
-        return true;
+        return false;
     }
 
+
+    class DownloadTaskRunnable implements Runnable {
+        private final DownloadTask _task;
+
+        public DownloadTaskRunnable(DownloadTask task) {
+            _task = task;
+        }
+
+        @Override
+        public void run() {
+            _task.ready();
+            try {
+                _task.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
